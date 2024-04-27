e9733baf0a728faaab93f85061d6a82bfee24ee3
kshitij2131 <jaiswal.8@iitj.ac.in>
2024-04-28 00:34:56 +0530

    .

--- pages/news.py
+++ pages/news.py
@@ -6,9 +6,12 @@ import requests
 
 
 dash.register_page(__name__, path='/news', name="RECENT NEWS")
+# api_key = "fad10f0941c470a19bf01b5f51419968"
+#In case the above api doesn't work, please use
+api_key = "87ae556dd203782a4fb6d957cd016698"
 
 # Loading the data..
-def fetch_tennis_news(start_date, end_date, api_key):
+def fetch_tennis_news(start_date, end_date):
     gnews_url = f"https://gnews.io/api/v4/search?q=tennis&from={start_date}&to={end_date}&lang=en&token={api_key}"
     print(gnews_url)
     response = requests.get(gnews_url)
@@ -48,8 +51,8 @@ def update_news_articles(date_range):
     start_date = start_date.strftime('%Y-%m-%dT%H:%M:%SZ')
     end_date = end_date.strftime('%Y-%m-%dT%H:%M:%SZ')
     # print(start_date, end_date)
-    api_key = "fad10f0941c470a19bf01b5f51419968"
-    articles = fetch_tennis_news(start_date, end_date, api_key)
+    
+    articles = fetch_tennis_news(start_date, end_date)
 
     if articles:
         news_list = []
